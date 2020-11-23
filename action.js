8c75668d319a683724e563729480b4aa9e0968ee
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-23 16:23:58 +0100

    Printing on screen music cards & loading check added

--- action.js
+++ action.js
@@ -1,10 +1,10 @@
 /* ASSIGNMENT
 Attraverso una chiamata ajax all’API di boolean
 https://flynn.boolean.careers/exercises/api/array/music
-
 avremo a disposizione una decina di dischi musicali.
-curare l'aspetto grafico.
 Utilizzando vue, stampiamo a schermo una card per ogni album.
+
+Curare l'aspetto grafico.
 BONUS 1: Creare una select con tutti i generi dei dischi.
 In base a cosa scegliamo nella select, vedremo i cd corrispondenti.
 BONUS 2: Ordinare i dischi per anno di uscita.
@@ -16,6 +16,8 @@ let app = new Vue({
   el: '#root',
   data: {
     title: 'Vue Music Discs',
+    discs_list: [],
+    discs_quantity: 10,
   },  // Closing data
   mounted: function() {
     // AJAX call
@@ -24,6 +26,7 @@ let app = new Vue({
     .then(ajax_call => {
       // Accessing the JSON object and its properties "data" & "response" containing the array (of objects) of music discs
       console.log(ajax_call.data.response);
+      this.discs_list = ajax_call.data.response;
     });
   },  // Closing mounted
 });
